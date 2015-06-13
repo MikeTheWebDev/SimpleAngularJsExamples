5a2e15dd9dfc2accc4b90f4d49a3fda814f6ea5a
MikeTheWebDev <MiketheWebDev@gmail.com>
2015-06-12 22:47:19 -0500

    Firebase sync

--- dashboard-app/app/scripts/controllers/manageProperties.js
+++ dashboard-app/app/scripts/controllers/manageProperties.js
@@ -1,11 +1,15 @@
 'use strict';
 
-angular.module('manageProperties', [])
+angular.module('manageProperties', ["firebase"])
     .controller('propertyCtrl', function($scope) {
         $scope.propertyName = "";
         $scope.propertyLocation = "";
+        // connect to firebase
         $scope.myData = new Firebase("https://crackling-inferno-8136.firebaseio.com/Properties");
-        
+        // what does this line even do?
+        $scope.fb = $firebase(myData);
+        // sync as object
+        $scope.syncObject = fb.$asObject();
         $scope.properties = {};
 
         $scope.saveProperty = function() {
@@ -18,8 +22,11 @@ angular.module('manageProperties', [])
         $scope.myData.on('value', function(snapshot) {
             $scope.properties = snapshot.val();
             $scope.$apply();
-        });            
+        });   
+        
+                 
 	
     })
    
+    
 ;
